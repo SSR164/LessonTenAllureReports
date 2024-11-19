9368c0b79894e692b76a39444d2aaae99e9e3da0
Serg Sh. <vampir6307@gmail.com>
2024-11-19 18:09:37 +0300

    holdBrowserOpen = false

--- LessonTenAllureReports/src/test/java/LessonTenBeforeAll.java
+++ LessonTenAllureReports/src/test/java/LessonTenBeforeAll.java
@@ -7,7 +7,7 @@ public class LessonTenBeforeAll {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com/";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
         Configuration.timeout = 5000; // default 4000
 
     }
